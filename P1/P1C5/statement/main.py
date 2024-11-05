55023d19f3fa8e88aa39a129025861cf75b81335
juliemgn <maguinidouaa@gmail.com>
2024-11-05 14:08:58 +0800

    Update main.py

--- P1/P1C5/statement/main.py
+++ P1/P1C5/statement/main.py
@@ -1,1 +1,15 @@
 # Write your code here!
+name = douaa maguini 
+age = 18
+height = 1.60 
+is_student = true
+
+print( " Name : " , name )
+print ( "Age : " , age )
+print (" Height : ", height )
+print ("Is student : " , is_student )
+
+print("Type name:" , type(name))
+print("Type age:" ,type(age))
+print("Type height:" ,type(height))
+print("Type is student:", type(is_student))
